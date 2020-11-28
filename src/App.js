533c947a91ef021dbdcc53b73b51418d24abb403
stefi23 <stefania.rosca2309@gmail.com>
2020-11-28 17:42:37 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay;
+  
+    if (hours < 12) {
+    timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    } else {
+      timeOfDay = "night"
+    }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Good {timeOfDay}!</h1>
+
     </div>
   );
 }
